0ad60a3e98c4361a3f8d6d5e05cf75c78c73ce83
autumndr3ams <autumndr3ams@gmail.com>
2023-03-05 00:33:46 +0900

    section11: useEffect를 사용해 cart에 항목 추가시 애니메이션

--- 02-adding-a-header-cmp/src/components/Layout/HeaderCartButton.js
+++ 02-adding-a-header-cmp/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,38 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext)
+
+  const {items} = cartCtx
+
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
     return curNumber + item.amount
   }, 0)
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(()=>{
+    if(items.length === 0){
+      // cart에 항목이 1개 이상 있을때만 애니메이션 추가하도록
+      return;
+    }
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
