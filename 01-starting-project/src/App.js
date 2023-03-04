c73bfa6be6a3b47703ca2b2ed78a69d9d062bc9d
autumndr3ams <autumndr3ams@gmail.com>
2023-03-04 10:38:16 +0900

    section10: useContext

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,41 +6,38 @@ import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './store/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(()=>{
-    // 모든 컴포넌트가 재평가 된 다음에 의존성이 변경된 경우에 실행됨
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+  // useEffect(()=>{
+  //   // 모든 컴포넌트가 재평가 된 다음에 의존성이 변경된 경우에 실행됨
+  //   const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
 
-    if(storedUserLoggedInInformation === '1'){
-      setIsLoggedIn(true)
-    }
-  }, []) // 의존성이 없으면 앱이 시작될 때 한 번만 실행됨
+  //   if(storedUserLoggedInInformation === '1'){
+  //     setIsLoggedIn(true)
+  //   }
+  // }, []) // 의존성이 없으면 앱이 시작될 때 한 번만 실행됨
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1') // 0: 로그아웃 1: 로그인
-    setIsLoggedIn(true);
-  };
+  // const loginHandler = (email, password) => {
+  //   // We should of course check email and password
+  //   // But it's just a dummy/ demo anyways
+  //   localStorage.setItem('isLoggedIn', '1') // 0: 로그아웃 1: 로그인
+  //   setIsLoggedIn(true);
+  // };
 
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn')
-    setIsLoggedIn(false);
-  };
+  // const logoutHandler = () => {
+  //   localStorage.removeItem('isLoggedIn')
+  //   setIsLoggedIn(false);
+  // };
+
+  const ctx = useContext(AuthContext)
 
   return (
     <React.Fragment>
-      <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-      }}>
-        <MainHeader onLogout={logoutHandler} />
+        <MainHeader/>
         <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
+          {!ctx.isLoggedIn && <Login />}
+          {ctx.isLoggedIn && <Home />}
         </main>
-      </AuthContext.Provider>
     </React.Fragment>
   );
 }
--- 01-starting-project/src/store/auth-context.js
+++ 01-starting-project/src/store/auth-context.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 // 컨텍스트 객체 생성
 const AuthContext = React.createContext({
-  isLoggedIn: false
+  isLoggedIn: false,
+  onLogout: ()=>{},
+  onLogin: (email, password)=>{}
 });
+
+export const AuthContextProvider = (props)=>{
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(()=>{
+    // 모든 컴포넌트가 재평가 된 다음에 의존성이 변경된 경우에 실행됨
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+
+    if(storedUserLoggedInInformation === '1'){
+      setIsLoggedIn(true)
+    }
+  }, []) // 의존성이 없으면 앱이 시작될 때 한 번만 실행됨
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true);
+  };
+
+  return (
+  <AuthContext.Provider 
+        value={{
+          isLoggedIn:isLoggedIn,
+          onLogout: logoutHandler, 
+          onLogin: loginHandler}
+        }>
+    {props.childern}
+  </AuthContext.Provider>
+  )
+}
 
 export default AuthContext;
