9a90cfac9f351a457d566fe34d63cd455d746bfb
autumndr3ams <autumndr3ams@gmail.com>
2023-03-04 22:55:51 +0900

    section11: 모달 추가

--- 02-adding-a-header-cmp/src/components/Layout/Header.js
+++ 02-adding-a-header-cmp/src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         {/* 이미지를 import 하면 됨 */}
