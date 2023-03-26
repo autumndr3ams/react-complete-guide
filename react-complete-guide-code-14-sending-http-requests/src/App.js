49c56544a1b5ddd030d6db2f847f424ba019f957
autumndr3ams <autumndr3ams@gmail.com>
2023-03-26 16:20:05 +0900

    section14: http post request

--- react-complete-guide-code-14-sending-http-requests/src/App.js
+++ react-complete-guide-code-14-sending-http-requests/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -12,35 +13,54 @@ function App() {
     setIsLoading(true)
     setError(null)
     try{
-      const response = await fetch('https://swapi.dev/api/films/')
+      const response = await fetch('https://react-udemy-9e79f-default-rtdb.firebaseio.com/movies.json')
       
       if(!response.ok){
         throw new Error('Something went wrong!')
       }
       
       const data = await response.json();
-      const transformedMovies = data.results.map(movie=>{
-        return{
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date
-        }
-      })
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch(error){
       setError(error.message)
     }
     setIsLoading(false)
     }, [])
 
-    useEffect(()=>{
-      // 컴포넌트가 최초 로딩 될 때 함수 실행
-      fetchMoviesHandler()
-    }, [fetchMoviesHandler]);
-    
+  useEffect(()=>{
+    // 컴포넌트가 최초 로딩 될 때 함수 실행
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler]);
+  
+  
+  async function addMovieHandler(movie){
+    const response = await fetch('https://react-udemy-9e79f-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log(data)
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
