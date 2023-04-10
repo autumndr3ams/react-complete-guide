38771af8cdb463068b79104c92b8d97912e8a7e4
autumndr3ams <autumndr3ams@gmail.com>
2023-04-10 23:23:38 +0900

    section 16: 기본 검증 & 에러메시지

--- react-complete-guide-code-16-working-with-forms/src/components/SimpleInput.js
+++ react-complete-guide-code-16-working-with-forms/src/components/SimpleInput.js
@@ -1,8 +1,9 @@
 import { useRef, useState } from 'react'
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState('')
   const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState('')
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value)
@@ -12,6 +13,13 @@ const SimpleInput = (props) => {
     //http request 전송 막기
     event.preventDefault()
 
+    // 기본적인 검증: input값이 비었을 때 return
+    if(enteredName.trim() === ''){
+      setEnteredNameIsValid(false)
+      return
+    }
+
+    setEnteredNameIsValid(true)
     // 키 입력마다 입력값이 필요하다면 state가 유리
     console.log(enteredName)
     // 제출된 값이 한 번만 필요하다면 ref가 유리
@@ -21,12 +29,15 @@ const SimpleInput = (props) => {
     setEnteredName('') // state는 초기화 가능, ref도 가능하지만 DOM을 직접 조작하는 것이므로 바람직하지 않음
   }
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         {/* 매 키마다 inputHandler 작동 */}
         <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler}/>
+        {!enteredNameIsValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
